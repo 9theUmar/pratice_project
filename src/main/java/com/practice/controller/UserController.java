ffd414d532aacd1548296d83af5bd4c79205092f
Saif khan <152763354+DeveloperSaif04@users.noreply.github.com>
2025-11-29 20:57:11 +0530

    user controller

---
+++ src/main/java/com/practice/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.practice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+    @PostMapping("/save")
+    public ResponseEntity<?> save(){
+        return null;
+    }
+}
