550bb75040b3cf803627d1e3d48f1385a0da0101
9theUmar <150515953+9theUmar@users.noreply.github.com>
2025-11-29 20:14:03 +0530

    first commit

---
+++ src/main/java/com/practice/entity/User.java
@@ -0,0 +1,40 @@
+package com.practice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(columnDefinition = "CHAR(36)")
+    private String uuid;
+
+    private String name;
+
+    private String mobile;
+
+    private String email;
+
+    private String city;
+
+    private String createdBy;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    private String updatedBy;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+
+}
